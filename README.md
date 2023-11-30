65e9511c58a47dc4a52d7c1adf0b80483f0f6546
Amey Jahagirdar <jahagird@amazon.com>
2023-11-30 12:46:53 +0530

    AltSvc behavior for Android WebView

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # AltSvcWebView
-AltSvc behavior for WebView
+
+This is meant to demonstrate the behavior with [Alt-Svc header](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Alt-Svc).
+
+This setup has Application that loads up the WebView and URL supporting Http3.
+
+
+# Steps to repro
+1. Monitor the logs using `adb logcat | grep -i WEB_VIEW_TEST`
+2. Run the application on a device. Let it load the website. Observe the value of the protocol. It will be Http2. Demo queries javascript using `performance.getEntriesByType('navigation')[0].nextHopProtocol` API to log this value.
+
+`
+I WEB_VIEW_TEST: Protocol used was "h2"
+`
+
+3. Now click on reload and observe the value of the protocol. It will be http3.
+
+`
+I WEB_VIEW_TEST: Protocol used was "h3"
+`
+
+
+3. Restart the app and observe the value of the protocol. It will be http2 and not http3.
+
+# How does other platform behave
+## Mac Canary - 121.0.6156.2
+## Android Chrome - 119.0.6045.134
+1. Open Mac Canary
+2. Visit any website that supports http3. i.e. https://m.youtube.com/
+3. Check the value of the protocol in Chrome DevTools or using `performance.getEntriesByType('navigation')[0].nextHopProtocol`. Ensure it is http3. Reload till it connects over http3.
+4. Close the browser completely. Reopen.
+5. Visit same site and check the protocol.
+6. It will be Http3 and not http2.
+
---
+++ app/src/main/java/com/example/altsvcwebview/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.altsvcwebview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        WebView.setWebContentsDebuggingEnabled(true);
+        webView.loadUrl("https://www.amazon.in");
+
+        findViewById(R.id.reload_webview_button).setOnClickListener(view -> {
+            webView.reload();
+        });
+
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                Log.i("WEB_VIEW_TEST", "onPageStarted url:" + url);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                view.evaluateJavascript("performance.getEntriesByType('navigation')[0].nextHopProtocol", new ValueCallback<String>() {
+                    @Override
+                    public void onReceiveValue(String value) {
+                        Log.i("WEB_VIEW_TEST", "Protocol used was " + value);
+                    }
+                });
+                super.onPageFinished(view, url);
+            }
+        });
+    }
+}
